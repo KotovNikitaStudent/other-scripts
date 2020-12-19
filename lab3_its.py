a219d603c950d802f1b3584d693835a057d85439
Nikita Kotov <49129570+KotovNikitaStudent@users.noreply.github.com>
2020-12-19 17:42:38 +0300

    Add files via upload

--- lab3_its.py
+++ lab3_its.py
@@ -1,15 +1,11 @@
 import numpy as np
 import random
 
-# alpha = float(input())
-# mu_1 = float(input())
-# mu_2 = float(input())
-# t_k = float(input())
-
 alpha = 5
 mu_1 = 1
 mu_2 = 3
 t_k = 24
+alpha_1 = 1
 
 t_c = 0
 n = 0
@@ -18,37 +14,37 @@ t_pr_1, t_pr_2 = 0, 0
 t_1, t_2, t_3, t_4 = 0, 0, 0, 0
 t = 0
 
-alpha_1 = 5
-# tc = []
 while t_c < t_k:
-    x = random.random()
-    r = alpha_1 - alpha_1*np.sqrt(1-x)
-    t_c += (1/alpha)*np.log(r)
+    # r = alpha_1 - alpha_1*np.sqrt(1-random.random())
+    r = random.random()
+    t_c = t_c - np.log(r)/alpha
     n += 1
     if t_c >= t_1:
-        # x = random.random()
-        # r = alpha_1 - alpha_1 * np.sqrt(1 - x)
-        t += (t_c - t_1)
-        t_1 = t_c - (1/mu_1)*np.log(r)
-        n_rej += 1
-        t_pr_1 += t_c - t_1
-    elif t_c >= t_2:
-        # x = random.random()
-        # r = alpha_1 - alpha_1 * np.sqrt(1 - x)
-        t += (t_c - t_2)
-        t_2 = t_c - (1/mu_2) * np.log(r)
-        n_rej += 1
-        t_pr_2 += t_c - t_2
-    elif t_c >= t_3:
-        t_3 = min(t_1, t_2)
-    elif t_c >= t_4:
-        t_4 = t_3
-    else:
+        # r = alpha_1 - alpha_1 * np.sqrt(1 - random.random())
+        r = random.random()
+        t = t + (t_c - t_1)
+        t_1 = t_c - np.log(r)/mu_1
         n_serv += 1
-        n_rej += 1
+        t_pr_1 = abs(t_pr_1 + t_c - t_1)
+    else:
+        if t_c >= t_2:
+            # r = alpha_1 - alpha_1 * np.sqrt(1 - random.random())
+            r = random.random()
+            t = t + (t_c - t_2)
+            t_2 = t_c - np.log(r)/mu_2
+            n_rej += 1
+            t_pr_2 = abs(t_pr_2 + t_c - t_2)
+        else:
+            if t_c >= t_3:
+                t_3 = min(t_1, t_2)
+            else:
+                if t_c >= t_4:
+                    t_4 = t_3
+                else:
+                    n_serv += 1
+                    n_rej += 1
 
-print('\n')
-print(f'производительность системы A = {n_serv/t_k}')
+print(f'\nпроизводительность системы A = {n_serv/t_k}')
 print(f'вероятность обслуживания Pобс = {n_serv/n}')
 print(f'вероятность отказа Pотк = {n_rej/n}')
 print(f'вероятность простоя 1 канала Pпр1 = {t_pr_1/t_k}')
