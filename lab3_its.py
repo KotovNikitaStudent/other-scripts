95de809cfba73f4c3f4df2388f6f0cf5d00f21a4
Nikita Kotov <49129570+KotovNikitaStudent@users.noreply.github.com>
2020-12-27 18:22:14 +0300

    Add files via upload

--- lab3_its.py
+++ lab3_its.py
@@ -1,51 +1,135 @@
 import numpy as np
-import random
+import random as rnd
+import matplotlib.pyplot as plt
 
-alpha = 5
-mu_1 = 1
-mu_2 = 3
-t_k = 24
-alpha_1 = 1
+kappa = 1
+time_model = 24
+len_queue = 3
+time_event_last = 0
+num_channel = 2
 
-t_c = 0
-n = 0
-n_serv, n_rej = 0, 0
-t_pr_1, t_pr_2 = 0, 0
-t_1, t_2, t_3, t_4 = 0, 0, 0, 0
-t = 0
+time_serv_sum = 0
+time_pr = 0
+time_in_queue = 0
+pos_queue = 0
 
-while t_c < t_k:
-    # r = alpha_1 - alpha_1*np.sqrt(1-random.random())
-    r = random.random()
-    t_c = t_c - np.log(r)/alpha
-    n += 1
-    if t_c >= t_1:
-        # r = alpha_1 - alpha_1 * np.sqrt(1 - random.random())
-        r = random.random()
-        t = t + (t_c - t_1)
-        t_1 = t_c - np.log(r)/mu_1
-        n_serv += 1
-        t_pr_1 = abs(t_pr_1 + t_c - t_1)
-    else:
-        if t_c >= t_2:
-            # r = alpha_1 - alpha_1 * np.sqrt(1 - random.random())
-            r = random.random()
-            t = t + (t_c - t_2)
-            t_2 = t_c - np.log(r)/mu_2
-            n_rej += 1
-            t_pr_2 = abs(t_pr_2 + t_c - t_2)
+time_event = []
+time_serv = []
+time_last_end = []
+ones_end = []
+ones_event = []
+
+class TEvent:
+    def __init__(self, _id, _label, _thread_id, _time_started):
+        self.id = _id
+        self.label = _label
+        self.thread_id = _thread_id
+        self.time_started = _time_started
+
+class TAvarege:
+    def __init__(self):
+        self.sum = 0
+        self.count = 0
+    def append(self, value):
+        self.sum += value
+        self.count += 1
+    def get(self):
+        if self.count == 0:
+            return 0
+        return self.sum/self.count
+
+class Statistics:
+    def __init__(self, num_channel):
+        self.n_rej = 0
+        self.n_serv = 0
+        self.time_eq = []
+        for i in range(num_channel):
+            self.time_eq.append(0)
+        self.avr_n_len = TAvarege()
+        self.time_free = 0
+        self.avr_time_task = TAvarege()
+        self.avr_task_in_sistem = TAvarege()
+        self.avr_len_task = TAvarege()
+        self.avr_time_in_serv = TAvarege()
+        self.avr_time_eq_in_system = TAvarege()
+
+    def print(self):
+        print(f' 1) Вероятность обслуживания = {self.n_serv/(self.n_serv + self.n_rej)}')
+        print(f' 2) Вероятность отказа = {self.n_rej/(self.n_serv + self.n_rej)}')
+        print(f' 3) Пропускная способность = {self.n_serv/time_model}')
+        print(f' 4) Среднее количество занятых каналов = {self.avr_len_task.get()}')
+        print(f' 5) Вероятность простоя всей системы = {self.time_free/time_model}')
+        print(f' 6) Среднее количество заявок в очереди = {self.avr_n_len.get()}')
+        print(f' 7) Среднее время ожидания заявки в очереди = {self.avr_time_task.get()}')
+        print(f' 8) Среднее время обслуживания заявки = {self.avr_time_in_serv.get()}')
+        print(f' 9) Среднее время нахождения заявки в системе = {self.avr_task_in_sistem.get()}')
+        print(f' 10) Среднее количество заявок в системе = {len_queue + self.n_serv/(self.n_serv + self.n_rej)}')
+
+class TThread:
+    def __init__(self, _label_thread, _power):
+        self.label_thread = _label_thread
+        self.power = _power
+
+def push_event(id,label,thread_id, time_started):
+	global time_event
+	t = TEvent(id,label,thread_id, time_started)
+	for i in range(0,len(time_event)):
+		if time_event[i].label > label:
+			v = time_event[:i]
+			v.append(t)
+			time_event = v + time_event[i:]
+			return
+	time_event.append(t)
+
+def gen_rnd(kappa):
+    return kappa*(1 - np.sqrt(1 - rnd.random()))
+
+current_time = 0
+push_event(1, gen_rnd(kappa), -1, 0)
+q = []
+thread = []
+for i in range(num_channel):
+    thread.append(TThread(-1, int(input(f'input thread power {i+1}: '))))
+
+stat = Statistics(num_channel)
+while current_time < time_model:
+    current = time_event.pop(0)
+    if current.id == 1:
+        push_event(1, current_time + gen_rnd(kappa), -1, current_time)
+        if len(q) < len_queue:
+            q.append(current)
+            stat.n_serv += 1
+            # print('task queued')
         else:
-            if t_c >= t_3:
-                t_3 = min(t_1, t_2)
-            else:
-                if t_c >= t_4:
-                    t_4 = t_3
-                else:
-                    n_serv += 1
-                    n_rej += 1
+            stat.n_rej += 1
+            # print('task NOT queued')
+    if current.id == 2:
+        thread[current.thread_id].label_thread = -1
+        # print('thread end')
+        stat.avr_time_task.append(current_time - current.time_started)
+    for i in range(0, len(thread)):
+        if thread[i].label_thread == -1:
+            if len(q) > 0:
+                thread[i].label_thread = current.label
+                stat.avr_time_in_serv.append(current_time - current.time_started)
+                push_event(2, current.label + thread[i].power, i, current_time)
+                q.pop(0)
+                # print('task threaded')
+                break
+    len_time = time_event[0].label - current_time
+    thread_free = 0
+    for i in range(0, len(thread)):
+        if thread[i].label_thread > 0:
+            stat.time_eq[i] += len_time
+        else:
+            thread_free += 1
+    if thread_free == num_channel:
+        stat.time_free += len_time
+    current_time = time_event[0].label
 
-print(f'\nпроизводительность системы A = {n_serv/t_k}')
-print(f'вероятность обслуживания Pобс = {n_serv/n}')
-print(f'вероятность отказа Pотк = {n_rej/n}')
-print(f'вероятность простоя 1 канала Pпр1 = {t_pr_1/t_k}')
-print(f'вероятность простоя 2 канала Pпр2 = {t_pr_2/t_k}')
+# stat.print()
+# plt.scatter(time_event, ones_event)
+# plt.scatter(time_last_end, ones_end)
+# plt.xlim([0, 2])
+# plt.grid(True)
+# plt.show()
